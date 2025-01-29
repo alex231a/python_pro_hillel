848c24c08dcd84517f1418cff627ae7e3d177f95
ayakovenko <ayakovenko@scalea.tech>
2025-01-29 10:50:06 +0200

    lesson5_HW_2

--- python/lesson5_hw/1_calc.py
+++ python/lesson5_hw/1_calc.py
@@ -5,33 +5,40 @@ decimal.getcontext().Emin = -5
 
 
 class UnknownOperationError(Exception):
+    """Custom Exception"""
     pass
 
 
 class Calc:
-    def __init__(self, num1, num2):
+    """Class calculator with main math methods"""
+
+    def __init__(self, num1: int, num2: int):
         self.num1 = decimal.Decimal(num1)
         self.num2 = decimal.Decimal(num2)
 
     def add(self):
+        """Adding"""
         try:
             return self.num1 + self.num2
         except decimal.Overflow:
             raise OverflowError("Result of addition caused an overflow")
 
     def sub(self):
+        """Subtraction"""
         try:
             return self.num1 - self.num2
         except decimal.Overflow:
             raise OverflowError("Result of subtraction caused an overflow")
 
     def mult(self):
+        """Mult"""
         try:
             return self.num1 * self.num2
         except decimal.Overflow:
             raise OverflowError("Result of multiplication caused an overflow")
 
     def div(self):
+        """Division"""
         if self.num2 == 0:
             raise ZeroDivisionError("Second argument cannot be 0")
         try:
@@ -40,7 +47,10 @@ class Calc:
             raise OverflowError("Result of division caused an overflow")
 
 
-def run_calculation(calculator, num1, num2, action):
+def run_calculation(calculator: Calc, num1: int, num2: int, action: str):
+    """Function that works with class Calc and call Calcs methods depends on
+    action"""
+
     calc = calculator(num1, num2)
 
     if action == "+":
@@ -56,11 +66,12 @@ def run_calculation(calculator, num1, num2, action):
 
 
 if __name__ == '__main__':
+    result = None
     try:
         num1 = input("Enter first value: ")
         num2 = input("Enter second value: ")
         action = input("Choose action (+, -, *, /): ").strip()
-        print(run_calculation(Calc, num1, num2, action))
+        result = run_calculation(Calc, num1, num2, action)
     except (ValueError, decimal.InvalidOperation):
         print(
             "(ValueError) - Invalid number input. Please enter a valid "
@@ -71,3 +82,6 @@ if __name__ == '__main__':
         print(f"(OverflowError) - {e}")
     except UnknownOperationError as e:
         print(f"(UnknownOperationError) - {e}")
+
+    if result is not None:
+        print(result)
---
+++ python/lesson5_hw/2_avgval.py
@@ -0,0 +1,38 @@
+class GetAvgValue:
+    """Class that reads data from file and count average values if all data
+    is integers"""
+
+    def __init__(self, filename: str):
+        self.filename = filename
+
+    def _read_file(self):
+        """Method for reading file"""
+        with open(self.filename, 'r') as file:
+            content = file.readlines()
+        return content
+
+    def get_avg_value(self):
+        """Method for counting average value"""
+        try:
+            content = self._read_file()
+            count_values = len(content)
+            content = [int(val) for val in content]
+            sum_values = sum(content)
+            avg_val = sum_values / count_values
+            return avg_val
+        except ValueError as e:
+            print(
+                f"File contains wrong values. ({e.__class__.__name__}) - {e}")
+        except FileNotFoundError as e:
+            print(f"({e.__class__.__name__}) - {e}")
+        except ZeroDivisionError as e:
+            print(
+                f"The file {self.filename} is empty. ("
+                f"{e.__class__.__name__})-{e}")
+
+
+if __name__ == "__main__":
+    calc = GetAvgValue('numbers.txt')
+    result = calc.get_avg_value()
+    if result is not None:
+        print(result)
